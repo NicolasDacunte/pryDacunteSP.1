7418f73df31f82e0176650d2d76407a3f9b6bc76
root <mgiraudo@ies21.edu.ar>
2023-08-28 13:59:32 -0300

    20230828-crear y grabar archivo

---
+++ pryDacunteSP.1/frmMain.Designer.cs
@@ -0,0 +1,86 @@
+﻿namespace pryDacunteSP._1
+{
+    partial class frmMain
+    {
+        /// <summary>
+        /// Variable del diseñador necesaria.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Limpiar los recursos que se estén usando.
+        /// </summary>
+        /// <param name="disposing">true si los recursos administrados se deben desechar; false en caso contrario.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Código generado por el Diseñador de Windows Forms
+
+        /// <summary>
+        /// Método necesario para admitir el Diseñador. No se puede modificar
+        /// el contenido de este método con el editor de código.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.btnCrear = new System.Windows.Forms.Button();
+            this.txtDatos = new System.Windows.Forms.TextBox();
+            this.btnGrabar = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // btnCrear
+            // 
+            this.btnCrear.Location = new System.Drawing.Point(97, 336);
+            this.btnCrear.Name = "btnCrear";
+            this.btnCrear.Size = new System.Drawing.Size(112, 48);
+            this.btnCrear.TabIndex = 0;
+            this.btnCrear.Text = "crear";
+            this.btnCrear.UseVisualStyleBackColor = true;
+            this.btnCrear.Click += new System.EventHandler(this.btnCrear_Click);
+            // 
+            // txtDatos
+            // 
+            this.txtDatos.Location = new System.Drawing.Point(47, 47);
+            this.txtDatos.Multiline = true;
+            this.txtDatos.Name = "txtDatos";
+            this.txtDatos.Size = new System.Drawing.Size(314, 113);
+            this.txtDatos.TabIndex = 1;
+            // 
+            // btnGrabar
+            // 
+            this.btnGrabar.Location = new System.Drawing.Point(249, 166);
+            this.btnGrabar.Name = "btnGrabar";
+            this.btnGrabar.Size = new System.Drawing.Size(112, 48);
+            this.btnGrabar.TabIndex = 2;
+            this.btnGrabar.Text = "Grabar";
+            this.btnGrabar.UseVisualStyleBackColor = true;
+            this.btnGrabar.Click += new System.EventHandler(this.btnGrabar_Click);
+            // 
+            // frmMain
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(384, 427);
+            this.Controls.Add(this.btnGrabar);
+            this.Controls.Add(this.txtDatos);
+            this.Controls.Add(this.btnCrear);
+            this.Name = "frmMain";
+            this.Text = "Manejo de Archivos y Carpetas";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button btnCrear;
+        private System.Windows.Forms.TextBox txtDatos;
+        private System.Windows.Forms.Button btnGrabar;
+    }
+}
+
---
+++ pryDacunteSP.1/frmMain.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+using System.IO;
+
+namespace pryDacunteSP._1
+{
+    public partial class frmMain : Form
+    {
+        public frmMain()
+        {
+            InitializeComponent();
+        }
+
+        private void btnCrear_Click(object sender, EventArgs e)
+        {
+
+            try
+            {
+                StreamWriter swCrearArchivo = new StreamWriter("miArchivito", false);
+                MessageBox.Show("Archivo Creado");
+            }
+            catch (Exception)
+            {
+                MessageBox.Show("Fatal Error");
+            }
+        }
+
+        private void btnGrabar_Click(object sender, EventArgs e)
+        {
+            try
+            {
+                StreamWriter swManejoArchivo = new StreamWriter("miArchivito", true);
+                swManejoArchivo.WriteLine(txtDatos.Text);
+                swManejoArchivo.Close();
+
+                MessageBox.Show("Almacenado");
+                txtDatos.Text = " ";
+                txtDatos.Focus();
+            }
+            catch (Exception)
+            {
+                MessageBox.Show("Fatal Error");
+            }
+        }
+    }
+}
